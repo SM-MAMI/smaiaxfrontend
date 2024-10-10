afb093cb182413091583e5a7d1406233a089fcc1
1r-1na <irij666@gmail.com>
2024-10-10 13:18:57 +0200

    refactor(login): reduced duplicated code in themePrimitives.ts

--- src/themes/themePrimitives.ts
+++ src/themes/themePrimitives.ts
@@ -188,59 +188,7 @@ export const getDesignTokens = (mode: PaletteMode) => {
         }),
       },
     },
-    typography: {
-      fontFamily: ['"Inter", "sans-serif"'].join(","),
-      h1: {
-        fontSize: defaultTheme.typography.pxToRem(48),
-        fontWeight: 600,
-        lineHeight: 1.2,
-        letterSpacing: -0.5,
-      },
-      h2: {
-        fontSize: defaultTheme.typography.pxToRem(36),
-        fontWeight: 600,
-        lineHeight: 1.2,
-      },
-      h3: {
-        fontSize: defaultTheme.typography.pxToRem(30),
-        lineHeight: 1.2,
-      },
-      h4: {
-        fontSize: defaultTheme.typography.pxToRem(24),
-        fontWeight: 600,
-        lineHeight: 1.5,
-      },
-      h5: {
-        fontSize: defaultTheme.typography.pxToRem(20),
-        fontWeight: 600,
-      },
-      h6: {
-        fontSize: defaultTheme.typography.pxToRem(18),
-        fontWeight: 600,
-      },
-      subtitle1: {
-        fontSize: defaultTheme.typography.pxToRem(18),
-      },
-      subtitle2: {
-        fontSize: defaultTheme.typography.pxToRem(14),
-        fontWeight: 500,
-      },
-      body1: {
-        fontSize: defaultTheme.typography.pxToRem(14),
-      },
-      body2: {
-        fontSize: defaultTheme.typography.pxToRem(14),
-        fontWeight: 400,
-      },
-      caption: {
-        fontSize: defaultTheme.typography.pxToRem(12),
-        fontWeight: 400,
-      },
-    },
-    shape: {
-      borderRadius: 8,
-    },
-    shadows: customShadows,
+    typography: typography,
   };
 };
 
@@ -401,5 +349,5 @@ export const shape = {
 };
 
 // @ts-ignore
-const defaultShadows: Shadows = ['var(--mui-palette-baseShadow)', ...defaultTheme.shadows.slice(1)];
+const defaultShadows: Shadows = ["var(--mui-palette-baseShadow)", ...defaultTheme.shadows.slice(1)];
 export const shadows = defaultShadows;
